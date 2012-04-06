3ba733551421070dbd7e3c0c97c670e7485e766a
DOITEAU Matthieu <doiteau.matthieu@free.fr>
2012-04-06 17:51:23 +0200

    attribute

--- JavaProject/src/model/impl/Moto.java
+++ JavaProject/src/model/impl/Moto.java
@@ -3,9 +3,35 @@ package model.impl;
 
 public class Moto extends AbstractVehicule {
 
+	private String marque;
+	private int puissanceFiscal;
+	
+	public Moto(String marque, int puissanceFiscal) {
+		this.marque = marque;
+		this.puissanceFiscal = puissanceFiscal;
+	}
+	
 	@Override
 	public String getName() {
-		return "2 Roues";
+		return "2 Roues (" + getMarque() + ", " + getPuissanceFiscal() + ")";
 	}
+
+	public String getMarque() {
+		return marque;
+	}
+
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+
+	public int getPuissanceFiscal() {
+		return puissanceFiscal;
+	}
+
+	public void setPuissanceFiscal(int puissanceFiscal) {
+		this.puissanceFiscal = puissanceFiscal;
+	}
+	
+	
 
 }
